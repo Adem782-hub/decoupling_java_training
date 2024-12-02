a754cf9e8073fbb887bd2e8fd92a51df95781f3d
Adem Lamkhanet <johndoe@example.fr>
2024-12-02 22:40:19 +0100

    Ajout de ContextualLogger et FileLogger pour les logs

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -1,8 +1,8 @@
 package fr.lernejo.logger;
 
 public class LoggerFactory {
-    public static Logger getLogger(String name) {
-        return new ConsoleLogger();
+    public static Logger getLogger(String callerClass) {
+        return new ContextualLogger(callerClass, new FileLogger("target/captain.log"));
     }
 }
 
